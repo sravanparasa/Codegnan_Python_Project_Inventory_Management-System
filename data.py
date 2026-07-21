eae259259836d123a5c9a5358bb2ab7860c334dd
sravanparasa <sravanparasa888@gmail.com>
2026-07-21 14:25:53 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,12 @@
+# Product Format:
+# [Product ID, Product Name, Category, Price, Quantity]
+
+products = [
+    ["P101", "Laptop", "Electronics", 55000, 10],
+    ["P102", "Mouse", "Accessories", 500, 25],
+    ["P103", "Keyboard", "Accessories", 1200, 15]
+]
+
+sales = []
+
+purchases = []
